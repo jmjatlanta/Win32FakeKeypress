da5a9aa8da6e43d1002e3f31feaeb30a2d3b9559
jmjatlanta <jmjatlanta@gmail.com>
2015-05-11 10:33:31 -0400

    added better comments

--- Win32FakeKeypress/Win32FakeKeypress.cpp
+++ Win32FakeKeypress/Win32FakeKeypress.cpp
@@ -1,5 +1,7 @@
-// Win32FakeKeypress.cpp : Defines the entry point for the application.
-//
+/*
+ * Win32FakeKeypress.cpp : If this application has focus, prevent the PC from going idle
+ * by introducing a keystroke on a timer.
+*/
 
 #include "stdafx.h"
 #include "Win32FakeKeypress.h"
@@ -12,7 +14,7 @@ HWND hWnd;
 TCHAR szTitle[MAX_LOADSTRING];					// The title bar text
 TCHAR szWindowClass[MAX_LOADSTRING];			// the main window class name
 
-// Forward declarations of functions included in this code module:
+// Forward declarations of functions and classes included in this code module:
 ATOM				MyRegisterClass(HINSTANCE hInstance);
 BOOL				InitInstance(HINSTANCE, int);
 LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -21,12 +23,22 @@ INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 bool endLoop = false;
 HANDLE threadHandle;
 
+/**
+ * used to wrap the HWND in a way that can be passed between threads
+ */
 struct thread_data
 {
 	HWND windowHandle;
+	// constructor
 	thread_data(HWND w) : windowHandle(w) {};
 };
 
+/**
+ * the loop for throwing keystrokes based on a timer
+ *
+ * @param lpParameter the wrapper around the window handle
+ * @returns always returns 0
+ */
 DWORD WINAPI doSeparateThreadLoop(LPVOID lpParameter)
 {
 	thread_data *data = (thread_data*)lpParameter;
@@ -60,23 +72,26 @@ DWORD WINAPI doSeparateThreadLoop(LPVOID lpParameter)
 
 }
 
+/**
+ * create a seperate thread for sending keystrokes into
+ * the keyboard buffer
+ */
 void doKeypressInSeperateThread()
 {
 	threadHandle = CreateThread(NULL, 0, doSeparateThreadLoop, new thread_data(hWnd), 0, 0);
 	return;
 }
 
-int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPTSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+/**
+ * The main() method
+ */
+int APIENTRY _tWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
 	UNREFERENCED_PARAMETER(hPrevInstance);
 	UNREFERENCED_PARAMETER(lpCmdLine);
 
- 	// TODO: Place code here.
-	MSG msg;
-	HACCEL hAccelTable;
+	MSG msg; // a place to store the message when one is received
+	HACCEL hAccelTable; // the keystroke accelerator table
 
 	// Initialize global strings
 	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
@@ -89,15 +104,16 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 		return FALSE;
 	}
 
-	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32FAKEKEYPRESS));
+	// load the keystroke accelerator table
+	hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WIN32FAKEKEYPRESS)); 
 
-	// this one starts a thread
+	// start the keypress thread
 	doKeypressInSeperateThread();
 
 	// Main message loop:
-	while (GetMessage(&msg, NULL, 0, 0))
+	while (GetMessage(&msg, NULL, 0, 0)) // wait for a message
 	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg)) // was this not an accelerator keystroke?
 		{
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
@@ -106,15 +122,15 @@ int APIENTRY _tWinMain(_In_ HINSTANCE hInstance,
 
 	endLoop = true;
 	
-
 	return (int) msg.wParam;
 }
 
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
+/**
+ * Build an object about this application
+ *
+ * @param hInstance this instance of the application
+ * @returns the return value of the RegisterClassEx call
+ */
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
 	WNDCLASSEX wcex;
@@ -136,15 +152,13 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 	return RegisterClassEx(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
+/**
+ * save the instance handle in a global variable and create and display the main program window.
+ *
+ * @param hInstance the instance handle
+ * @param nCmdShow ??
+ * @return TRUE if window creation was successful
+ */
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
    hInst = hInstance; // Store instance handle in our global variable
@@ -162,16 +176,18 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
    return TRUE;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE:  Processes messages for the main window.
-//
-//  WM_COMMAND	- process the application menu
-//  WM_PAINT	- Paint the main window
-//  WM_DESTROY	- post a quit message and return
-//
-//
+/**
+ * Processes messages for the main window.
+ *
+ * @param hWnd the window handle
+ * @param message the message, one of
+ *   WM_COMMAND	- process the application menu
+ *   WM_PAINT	- Paint the main window
+ *   WM_DESTROY	- post a quit message and return
+ * @param wParam more parameters from the message
+ * @param lParam more parameters from the message
+ * @return 0 if message was handled, otherwise a non-zero value
+ */
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	int wmId, wmEvent;
@@ -210,24 +226,31 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-//
-// Message handler for the about box
-//
+/**
+ * message handler for the About box
+ *
+ * @param hDlg the handle to the dialog box
+ * @param message the message coming in
+ * @param wParam the parameter
+ * @param lParam not used
+ * 
+ * @returns integer equivalent of TRUE (message was handled) or FALSE (message still needs to be handled)
+ */
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	UNREFERENCED_PARAMETER(lParam);
 	switch (message)
 	{
-	case WM_INITDIALOG:
-		return (INT_PTR)TRUE;
-
-	case WM_COMMAND:
-		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-		{
-			EndDialog(hDlg, LOWORD(wParam));
+		case WM_INITDIALOG:
 			return (INT_PTR)TRUE;
-		}
-		break;
+
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+			{
+				EndDialog(hDlg, LOWORD(wParam));
+				return (INT_PTR)TRUE;
+			}
+			break;
 	}
 	return (INT_PTR)FALSE;
 }
